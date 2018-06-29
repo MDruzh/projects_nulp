73ba68b83d69af401e76b3c102330b091e400365
Maksym <druzh.maks@gmail.com>
2018-06-29 12:17:25 +0300

    Java_Lab4
    Java_Lab4

---
+++ Java_Lab4/src/ua/lviv/iot/Main.java
@@ -0,0 +1,14 @@
+package ua.lviv.iot;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringProcessor stringProcessor = new StringProcessor();
+
+        String inputString = stringProcessor.readInputText();
+        String resultText = stringProcessor.processText(inputString);
+        stringProcessor.showResults(resultText);
+    }
+}
